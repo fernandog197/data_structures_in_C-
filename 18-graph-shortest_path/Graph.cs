1076cb5f0bb2c3a507f06c252ba82f461e59875b
fernandog197 <bluewavesalta@gmail.com>
2023-05-08 17:46:11 -0300

    18-graph-shortest_path INCOMPLETO

---
+++ 18-graph-shortest_path/Graph.cs
@@ -0,0 +1,122 @@
+namespace GraphShortestPath
+{
+    class Graph
+    {
+        int[,] myAdjacent;
+        int[] inDegree;
+        int nodes;
+
+        public Graph (int nodes)
+        {
+            this.nodes = nodes;
+            this.myAdjacent = new int[nodes, nodes];
+            this.inDegree = new int[nodes];
+        }
+
+        public void AddEdge (int firstNode, int lastNode)
+        {
+            myAdjacent[firstNode, lastNode] = 1;
+        }
+
+        public void printAdjacency ()
+        {
+            int n = 0;
+            int m = 0;
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            for(n=0; n<this.nodes; n++)
+            {
+                Console.Write("\t{0}", n);
+            }
+
+            Console.WriteLine();
+
+            for(n=0; n<this.nodes; n++)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(n);
+
+                for(m=0; m<this.nodes; m++)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write("\t{0}", myAdjacent[n, m]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public int getAdjancent (int row, int column)
+        {
+            return myAdjacent[row, column];
+        }
+
+        public void calculateInDegree ()
+        {
+            int n = 0;
+            int m = 0;
+
+            for(n=0; n<this.nodes; n++)
+            {
+                for(m=0; m<this.nodes; m++)
+                {
+                    if(myAdjacent[m, n] == 1)
+                    {
+                        inDegree[n]++;
+                    }
+                }
+            }
+        }
+
+        public void printInDegree ()
+        {
+            int n = 0;
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+            for(n=0; n<this.nodes; n++)
+            {
+                Console.WriteLine("Nodo: [{0}][{1}]", n, inDegree[n]);
+            }
+        }
+
+        public int foundZeroInDegree ()
+        {
+            bool found = false;
+            int n = 0;
+
+            for(n=0; n<this.nodes; n++)
+            {
+                if(inDegree[n] == 0)
+                {
+                    found = true;
+                    break;
+                }
+            }
+
+            if(found)
+            {
+                return n;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        public void decreaseInDegree (int node)
+        {
+            inDegree[node] = -1;
+
+            int n = 0;
+
+            for(n=0; n<this.nodes; n++)
+            {
+                if(myAdjacent[node, n] == 1)
+                {
+                    inDegree[n]--;
+                }
+            }
+        }
+    }
+}
---
+++ 18-graph-shortest_path/Program.cs
@@ -0,0 +1,118 @@
+﻿namespace GraphShortestPath
+{
+    class Program
+    {
+        public static void Main ()
+        {
+            int start = 0;
+            int end = 0;
+            int distance = 0;
+            int n = 0;
+            int m = 0;
+            int nodesNumber = 7;
+            string data = "";
+
+            Graph myGraph = new Graph(nodesNumber);
+
+            myGraph.AddEdge(0, 1);
+            myGraph.AddEdge(0, 2);
+            myGraph.AddEdge(0, 3);
+            myGraph.AddEdge(1, 3);
+            myGraph.AddEdge(1, 4);
+            myGraph.AddEdge(2, 5);
+            myGraph.AddEdge(3, 2);
+            myGraph.AddEdge(3, 5);
+            myGraph.AddEdge(3, 6);
+            myGraph.AddEdge(4, 3);
+            myGraph.AddEdge(4, 6);
+            myGraph.AddEdge(6, 5);
+
+            myGraph.printAdjacency();
+
+            //En aplicaciones reales es neceario implementar medidas de seguridad, por ejemplo, verificar los indices.
+            Console.WriteLine("Dame el nodo de inicio: ");
+            data = Console.ReadLine();
+            start = Convert.ToInt32(data);
+
+            Console.WriteLine("Dame el nodo final: ");
+            data = Console.ReadLine();
+            end = Convert.ToInt32(data);
+
+            /*
+            Creamos la tabla:
+            0 - Visitado
+            1 - Distancia
+            2 - Previo
+            */
+            int[,] board = new int[nodesNumber, 3];
+
+            //Inicializamos la tabla
+            for(n=0; n<nodesNumber; n++)
+            {
+                board[n, 0] = 0;
+                board[n, 1] = int.MaxValue;
+                board[n, 2] = 0;
+            }
+
+            board[start, 1] = 0;
+
+            PrintBoard(board);
+
+            for(distance=0; distance<nodesNumber; distance++)
+            {
+                for(n=0; n<nodesNumber; n++)
+                {
+                    if((board[n, 0] == 0) && (board[n, 1] == distance))
+                    {
+                        board[n, 0] = 1;
+
+                        for(m=0; m<nodesNumber; m++)
+                        {
+                            if(myGraph.getAdjancent(n, m) == 1)
+                            {
+                                if(board[m ,1] == int.MaxValue)
+                                {
+                                    board[m, 1] = distance + 1;
+                                    board[m, 2] = n;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            PrintBoard(board);
+
+            List<int> route = new List<int>();
+            int node01 = end;
+
+            while(node01 != start)
+            {
+                route.Add(node01);
+                node01 = board[node01, 2];
+            }
+
+            route.Add(start);
+            route.Reverse();
+
+            foreach(int position in route)
+            {
+                Console.Write("{0} -> ", position);
+            }
+
+            Console.WriteLine();
+        }
+
+        public static void PrintBoard (int[,] board)
+        {
+            int n = 0;
+
+            for(n=0; n<board.GetLength(0); n++)
+            {
+                Console.WriteLine("{0} -> {1}\t{2}\t{3}", n, board[n, 0], board[n, 1], board[n, 2]);
+            }
+
+            Console.WriteLine("------");
+        }
+    }
+}
